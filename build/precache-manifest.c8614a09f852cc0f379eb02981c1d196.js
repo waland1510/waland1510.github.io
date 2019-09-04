d972bc2e94245d42bfc6b5c9840c78f78ee58fed
Valeriy Andrushchak <waland1510@gmail.com>
2019-09-04 11:27:01 -0400

    build

---
+++ build/precache-manifest.c8614a09f852cc0f379eb02981c1d196.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3244470f1c91f94b035bcf470db465",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0fac78b194948a1ea70",
+    "url": "/static/css/main.84facf53.chunk.css"
+  },
+  {
+    "revision": "45c81aec683a7f2e3546",
+    "url": "/static/js/2.e0645e80.chunk.js"
+  },
+  {
+    "revision": "e0fac78b194948a1ea70",
+    "url": "/static/js/main.1848f294.chunk.js"
+  },
+  {
+    "revision": "9fddbb50441e6791a55c",
+    "url": "/static/js/runtime~main.e159818f.js"
+  }
+]);
