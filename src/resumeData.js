b20e825ee41cacac1ee44fb4a385ce0ee312ead3
Valeriy Andrushchak <waland1510@gmail.com>
2019-10-23 10:34:23 -0400

    Add ReactTracks App

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,125 +1,130 @@
 let resumeData = {
-  "imagebaseurl":"https://waland1510.github.io/",
-  "name": "Valeriy Andrushchak",
-  "role": "Full-Stack Software Developer",
-  "linkedinId":"Your LinkedIn Id",
-  "skypeid": "Your skypeid",
-  "roleDescription": "",
-  "socialLinks":[
-      { 
-        "id":"1",
-        "name":"linkedin",
-        "url":"https://www.linkedin.com/in/valeriyandrushchak/",
-        "className":"fa fa-linkedin"
-      },
-      {
-        "id":"2",
-        "name":"github",
-        "url":"https://github.com/waland1510",
-        "className":"fa fa-github"
-      }
-      // {
-      //   "name":"skype",
-      //   "url":"http://skype.com",
-      //   "className":"fa fa-skype"
-      // }
-    ],
-  "aboutme":"I am studying on the Enterprise Web and Mobile Development full-time program at Trios College. I have a love for software development and programming and I can see myself excelling in this industry. I am very passionate about becoming Full-Stack Software Developer as my skills include both backend and frontend development and I have deep curiosity in existing and emerging web technologies and enjoy learning new things. My business administration and management degree together with the experience of being banking corporate account manager for more than 10 years, despite being in a different industry, has equipped me with problem-solving and analytical skills that are so beneficial as a developer",
-  "address":"Toronto, Canada",
-  "website":"https://waland1510.github.io/",
-  "education":[
+  imagebaseurl: "https://waland1510.github.io/",
+  name: "Valeriy Andrushchak",
+  role: "Full-Stack Software Developer",
+  linkedinId: "Your LinkedIn Id",
+  skypeid: "Your skypeid",
+  roleDescription: "",
+  socialLinks: [
     {
-      "id":"1",
-      "UniversityName":"Trios College",
-      "specialization":"Enterprise Web and Mobile Development",
-      "MonthOfPassing":"2018 - ",
-      "YearOfPassing":"2019",
-      "Achievements":""
+      id: "1",
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/valeriyandrushchak/",
+      className: "fa fa-linkedin"
     },
     {
-      "id":"2",
-      "UniversityName":"Steinbeis University Berlin",
-      "specialization":"Master of Business Administration",
-      "MonthOfPassing":"2010 - ",
-      "YearOfPassing":"2012",
-      "Achievements":""
+      id: "2",
+      name: "github",
+      url: "https://github.com/waland1510",
+      className: "fa fa-github"
+    }
+    // {
+    //   "name":"skype",
+    //   "url":"http://skype.com",
+    //   "className":"fa fa-skype"
+    // }
+  ],
+  aboutme:
+    "I am studying on the Enterprise Web and Mobile Development full-time program at Trios College. I have a love for software development and programming and I can see myself excelling in this industry. I am very passionate about becoming Full-Stack Software Developer as my skills include both backend and frontend development and I have deep curiosity in existing and emerging web technologies and enjoy learning new things. My business administration and management degree together with the experience of being banking corporate account manager for more than 10 years, despite being in a different industry, has equipped me with problem-solving and analytical skills that are so beneficial as a developer",
+  address: "Toronto, Canada",
+  website: "https://waland1510.github.io/",
+  education: [
+    {
+      id: "1",
+      UniversityName: "Trios College",
+      specialization: "Enterprise Web and Mobile Development",
+      MonthOfPassing: "2018 - ",
+      YearOfPassing: "2019",
+      Achievements: ""
+    },
+    {
+      id: "2",
+      UniversityName: "Steinbeis University Berlin",
+      specialization: "Master of Business Administration",
+      MonthOfPassing: "2010 - ",
+      YearOfPassing: "2012",
+      Achievements: ""
     }
   ],
-  "work":[
+  work: [
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
-      "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements"
     },
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
-      "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements"
     }
   ],
-  "skillsDescription":"",
-  "skills":[
+  skillsDescription: "",
+  skills: [
     {
-      "id":"1",
-      "skillname":"HTML5"
+      id: "1",
+      skillname: "HTML5"
     },
     {
-      "id":"2",
-      "skillname":"CSS"
+      id: "2",
+      skillname: "CSS"
     },
     {
-      "id":"3",
-      "skillname":"Reactjs"
+      id: "3",
+      skillname: "Reactjs"
     },
     {
-      "id":"4",
-      "skillname":"Node"
+      id: "4",
+      skillname: "Node"
     },
     {
-      "id":"5",
-      "skillname":"Python"
+      id: "5",
+      skillname: "Python"
     }
   ],
-  "portfolio":[
-    { "id":"1",
-      "name":"Robofriends",
-      "description":"React App",
-      "imgurl":"images/portfolio/robofriends.png",
-      "appurl":"https://robofriends-redux-va.herokuapp.com"
+  portfolio: [
+    {
+      id: "1",
+      name: "Robofriends",
+      description: "React App",
+      imgurl: "images/portfolio/robofriends.png",
+      appurl: "https://robofriends-redux-va.herokuapp.com"
     },
-    { "id":"2",
-      "name":"Youtube Search",
-      "description":"React App",
-      "imgurl":"images/portfolio/youtube-search.png",
-      "appurl":"https://triosdevelopers.com/~V.Andrushchak/youtube-searc"
+    {
+      id: "2",
+      name: "Youtube Search",
+      description: "React App",
+      imgurl: "images/portfolio/youtube-search.png",
+      appurl: "https://triosdevelopers.com/~V.Andrushchak/youtube-searc"
     },
-    { "id":"3",
-      "name":"The Venue",
-      "description":"React App",
-      "imgurl":"images/portfolio/the-venue.png",
-      "appurl":"https://the-venue-va.herokuapp.com"
+    {
+      id: "3",
+      name: "React Tracks",
+      description: "Django React GraphQL",
+      imgurl: "images/portfolio/ReactTracks.png",
+      appurl: "https://django-react-gql.herokuapp.com"
     },
-    { "id":"4",
-      "name":"DevConnector",
-      "description":"React App",
-      "imgurl":"images/portfolio/devconnector.png",
-      "appurl":"https://fathomless-taiga-21220.herokuapp.com"
+    {
+      id: "4",
+      name: "DevConnector",
+      description: "React App",
+      imgurl: "images/portfolio/devconnector.png",
+      appurl: "https://fathomless-taiga-21220.herokuapp.com"
     }
   ],
-  "testimonials":[
+  testimonials: [
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
     },
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      description: "This is a sample testimonial",
+      name: "Some technical guy"
     }
   ]
-}
+};
 
-export default resumeData
+export default resumeData;
